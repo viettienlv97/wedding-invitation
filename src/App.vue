2dd86bf0348711aba2e173d83328d257aa16921c
Viet Tien <viettienvu.97@gmail.com>
2023-11-13 00:00:40 +0700

    nocmt

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MainTemplate/>
+  <TimeLine/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainTemplate from './components/MainTemplate.vue'
+import TimeLine from './components/TimeLine.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainTemplate,
+    TimeLine
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'FzBefront', serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0px;
 }
 </style>
---
+++ src/components/TimeLine.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="wrapper">
+        <div class="row h-100 align-items-center justify-content-center">
+            <div class="col-sm-12 col-md-6 center-img">
+                <img style="max-width: 65%;" src="../../public/img/time-line.png" alt="">
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+    .wrapper {
+        height: 100vh;
+
+        .center-img {
+            position: relative;
+
+            .center-texts {
+                position: absolute;
+                top: 33%;
+                left: 50%;
+                transform: translate(-50%, -47%);
+
+                .text {
+                    font-weight: 700;
+                    color: #bc8670;
+                }
+            }
+        }
+    }
+</style>
